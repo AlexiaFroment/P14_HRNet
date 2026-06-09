d9b47834f44bc03e004755e8eabee4cbd9f42293
AloaCmoi <aloacmoi@gmail.com>
2026-06-09 15:42:11 +0200

    add github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,45 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import { visualizer } from "rollup-plugin-visualizer";
-import { resolve } from "path";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-var __dirname = dirname(fileURLToPath(import.meta.url));
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
+import { visualizer } from "rollup-plugin-visualizer"
+import { resolve } from "path"
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+var __dirname = dirname(fileURLToPath(import.meta.url))
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-        react({
-            babel: {
-                plugins: [
-                    [
-                        "babel-plugin-import",
-                        {
-                            libraryName: "antd",
-                            libraryDirectory: "es",
-                            style: false,
-                        },
-                    ],
-                ],
+  base: "/P14_HRNet/",
+  plugins: [
+    tailwindcss(),
+    react({
+      babel: {
+        plugins: [
+          [
+            "babel-plugin-import",
+            {
+              libraryName: "antd",
+              libraryDirectory: "es",
+              style: false,
             },
+          ],
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        visualizer({
+          filename: "stats.html",
+          open: true,
+          gzipSize: true,
+          brotliSize: true,
         }),
-    ],
-    resolve: {
-        alias: {
-            "@": resolve(__dirname, "src"),
-        },
+      ],
     },
-    build: {
-        rollupOptions: {
-            plugins: [
-                visualizer({
-                    filename: "stats.html",
-                    open: true,
-                    gzipSize: true,
-                    brotliSize: true,
-                }),
-            ],
-        },
-    },
-});
+  },
+})
