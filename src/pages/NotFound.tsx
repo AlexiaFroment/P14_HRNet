7dbaaeeaea7d6795162ebf24665ff08b334b0e8a
AloaCmoi <aloacmoi@gmail.com>
2026-06-13 14:08:15 +0200

    update notfound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -5,12 +5,12 @@ const NotFound: React.FC = () => {
   return (
     <>
       <Title
-        text="⛔ Cette page n'existe pas ⛔"
+        text='🚧 Cette page est en cours de construction 🚧'
         level={1}
         className='text-red-500'
       />
       <div className='text-center text-red-500'>
-        Vous pouvez retrouver la liste des employés{" "}
+        Retrouvez la liste des employés{" "}
         <NavLink to='/current-employees' label='ici' />.
       </div>
     </>
